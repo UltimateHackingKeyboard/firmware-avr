5ee024ab1773de7f397acafaa97109809687db08
László Monda <laci@monda.hu>
2014-01-23 18:32:32 +0100

    Set SS as output to make master SPI mode work.  As a result exchange PB2 and PD2.  Extract SPI macros.

--- left/uhk-left.c
+++ left/uhk-left.c
@@ -7,19 +7,14 @@
 
 KeyMatrix_t KeyMatrixLeft;
 
-char SpiTransmit(char data)
+void SpiTransmit(char data)
 {
     SPDR = data;
-
-    // TODO: Investigate on why the following loop blocks and uncomment it eventually.
-    // while (!(SPSR & (1<<SPIF))) {
-    // }
+    while (!(SPSR & (1<<SPIF)));
 
     // Copy data to the storage register by toggling the latch.
-    PORTC |= (1<<PC3);
-    PORTC &= ~(1<<PC3);
-
-    return SPDR;
+    PORT_RCK |= (1<<PD_RCK);
+    PORT_RCK &= ~(1<<PD_RCK);
 }
 
 uint8_t SetColCallback(uint8_t col)
@@ -39,7 +34,7 @@ const __flash KeyMatrixInfo_t KeyMatrixInfoLeft = {
     .RowPins = (Pin_t[]) {
         { .Direction=&DDRB, .Name=&PINB, .Number=PINB0 },
         { .Direction=&DDRB, .Name=&PINB, .Number=PINB1 },
-        { .Direction=&DDRB, .Name=&PINB, .Number=PINB2 },
+        { .Direction=&DDRD, .Name=&PIND, .Number=PIND2 },
         { .Direction=&DDRC, .Name=&PINC, .Number=PINC1 },
         { .Direction=&DDRC, .Name=&PINC, .Number=PINC2 },
     },
@@ -47,7 +42,7 @@ const __flash KeyMatrixInfo_t KeyMatrixInfoLeft = {
         { .Direction=&DDRD, .Name=&PORTD, .Number=PORTD4 },
         { .Direction=&DDRD, .Name=&PORTD, .Number=PORTD3 },
         // Column 3 is controlled by DRAIN7 of the TPIC6C595 power shift register.
-        { .Direction=0,     .Name=0,      .Number=0 },
+        { .Direction=0,     .Name=0,      .Number=0      },
         { .Direction=&DDRC, .Name=&PORTC, .Number=PORTC0 },
         { .Direction=&DDRD, .Name=&PORTD, .Number=PORTD7 },
         { .Direction=&DDRD, .Name=&PORTD, .Number=PORTD6 },
@@ -61,12 +56,13 @@ int main(void)
     USART_Init();
 
     /* Initialize SPI */
-    DDRD |= 1<<DDD2;  // OE is output.
-    DDRB |= 1<<DDB5;  // SCK is output.
-    DDRC |= 1<<DDC3;  // RCK is output.
-    SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0) | (1<<CPOL) | (1<<CPHA);  // SPI enable, Master, f/16
+    DDR_MOSI |= 1<<DDB_MOSI; // MOSI is output.
+    DDR_OE   |= 1<<DDB_OE;   // OE is output.
+    DDR_SCK  |= 1<<DDB_SCK;  // SCK is output.
+    DDR_RCK  |= 1<<DDB_RCK;  // RCK is output.
+    SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0) | (1<<SPR1);  // SPI enable, Master, f/16
     SpiTransmit(0);
-    PORTD &= ~(1<<PD2);  // Enable OE by pulling it low.
+    PORT_OE &= ~(1<<PD_OE);  // Enable OE by pulling it low.
 
     KeyMatrix_Init(&KeyMatrixLeft, &KeyMatrixInfoLeft);
 
--- left/uhk-left.h
+++ left/uhk-left.h
@@ -9,4 +9,19 @@
         #include "KeyMatrix.h"
         #include "USART.h"
 
+    /* Macros: */
+        #define DDR_MOSI DDRB
+        #define DDB_MOSI DDB3
+        #define DDR_OE   DDRB
+        #define DDB_OE   DDB2
+        #define DDR_SCK  DDRB
+        #define DDB_SCK  DDB5
+        #define DDR_RCK  DDRC
+        #define DDB_RCK  DDC3
+
+        #define PORT_OE  PORTB
+        #define PD_OE    PB2
+        #define PORT_RCK PORTC
+        #define PD_RCK   PC3
+
 #endif
