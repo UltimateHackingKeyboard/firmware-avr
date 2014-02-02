c2b065e635a3b84b8673556c75d6ca82342613d8
László Monda <laci@monda.hu>
2014-02-02 19:51:04 +0100

    Rename KEY_STATE_MASK_PREV to KEY_STATE_MASK_PREVIOUS.

--- KeyState.h
+++ KeyState.h
@@ -3,11 +3,11 @@
 
     /* Macros: */
         #define KEY_STATE_MASK_CURRENT    (1 << 0)
-        #define KEY_STATE_MASK_PREV       (1 << 1)
+        #define KEY_STATE_MASK_PREVIOUS   (1 << 1)
         #define KEY_STATE_MASK_SUPPRESSED (1 << 2)
 
         #define KEY_STATE_IS_PRESSED(KeyState)    (KeyState & KEY_STATE_MASK_CURRENT)
-        #define KEY_STATE_WAS_PRESSED(KeyState)   (KeyState & KEY_STATE_MASK_PREV)
+        #define KEY_STATE_WAS_PRESSED(KeyState)   (KeyState & KEY_STATE_MASK_PREVIOUS)
         #define KEY_STATE_IS_SUPPRESSED(KeyState) (KeyState & KEY_STATE_MASK_SUPPRESSED)
 
         #define KEY_STATE_BECAME_PRESSED(KeyState) \
@@ -19,7 +19,7 @@
 
         #define CONSTRUCT_KEY_STATE(PrevKeyState, CurrentKeyState, SuppressedKeyState) \
             ((CurrentKeyState    ? KEY_STATE_MASK_CURRENT    : 0) | \
-             (PrevKeyState       ? KEY_STATE_MASK_PREV       : 0) | \
+             (PrevKeyState       ? KEY_STATE_MASK_PREVIOUS   : 0) | \
              (SuppressedKeyState ? KEY_STATE_MASK_SUPPRESSED : 0))
 
 #endif
