6e849216d0f48bb7138b35c17039ecf998a8f433
László Monda <laci@monda.hu>
2014-02-02 14:24:15 +0100

    Clean up KeyState macros.

--- KeyState.h
+++ KeyState.h
@@ -2,16 +2,18 @@
 #define KEY_STATE_H
 
     /* Macros: */
-        #define KEY_STATE_MASK_CURRENT    0b001
-        #define KEY_STATE_MASK_PREV       0b010
-        #define KEY_STATE_MASK_SUPPRESSED 0b100
+        #define KEY_STATE_MASK_CURRENT    (1 << 0)
+        #define KEY_STATE_MASK_PREV       (1 << 1)
+        #define KEY_STATE_MASK_SUPPRESSED (1 << 2)
 
         #define GET_KEY_STATE_CURRENT(KeyState)    (KeyState & KEY_STATE_MASK_CURRENT)
         #define GET_KEY_STATE_PREV(KeyState)       (KeyState & KEY_STATE_MASK_PREV)
         #define GET_KEY_STATE_SUPPRESSED(KeyState) (KeyState & KEY_STATE_MASK_SUPPRESSED)
 
         #define CONSTRUCT_KEY_STATE(PrevKeyState, CurrentKeyState, SuppressedKeyState) \
-            ((CurrentKeyState ? KEY_STATE_MASK_CURRENT : 0) | (PrevKeyState ? KEY_STATE_MASK_PREV : 0) | (SuppressedKeyState ? KEY_STATE_MASK_SUPPRESSED : 0))
+            ((CurrentKeyState    ? KEY_STATE_MASK_CURRENT    : 0) | \
+             (PrevKeyState       ? KEY_STATE_MASK_PREV       : 0) | \
+             (SuppressedKeyState ? KEY_STATE_MASK_SUPPRESSED : 0))
 
         #define KEY_STATE_PRESSED  0b11
         #define KEY_STATE_RELEASED 0b00
