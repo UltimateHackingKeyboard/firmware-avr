4c3e79c852dc1bbbdfe3c35e020702f475ee2b8c
László Monda <laci@monda.hu>
2014-02-02 11:33:55 +0100

    Change terminology from "keymap" to "layer".

--- right/Keyboard.h
+++ right/Keyboard.h
@@ -12,10 +12,10 @@
         #include "KeyboardReports.h"
 
     /* Macros: */
-        #define KEYMAP_ID_NORMAL 0
-        #define KEYMAP_ID_MOD    1
-        #define KEYMAP_ID_FN     2
-        #define KEYMAP_ID_MOUSE  3
+        #define LAYER_ID_NORMAL 0
+        #define LAYER_ID_MOD    1
+        #define LAYER_ID_FN     2
+        #define LAYER_ID_MOUSE  3
 
         #define KEY_ACTION   0
         #define KEY_ARGUMENT 1
@@ -24,22 +24,22 @@
         #define NO_ARGUMENT 0
 
         #define VIRUTAL_MODIFIER_TYPES_NUM 3  // Mod, Fn and Mouse
-        #define KEYMAPS_NUM (VIRUTAL_MODIFIER_TYPES_NUM + 1)
+        #define LAYERS_NUM (VIRUTAL_MODIFIER_TYPES_NUM + 1)
 
         #define ITEM_NUM_PER_KEY 2  // bytes
 
         /* The following values must not collide with any of the HID_KEYBOARD_SC_* constants of LUFA! */
-        #define VIRTUAL_MODIFIER_KEY_NONE  0x00
-        #define VIRTUAL_MODIFIER_KEY_MOD   0xE8
-        #define VIRTUAL_MODIFIER_KEY_FN    0xE9
-        #define VIRTUAL_MODIFIER_KEY_MOUSE 0xEA
+        #define LAYER_SWITCHER_KEY_NONE  0x00
+        #define LAYER_SWITCHER_KEY_MOD   0xE8
+        #define LAYER_SWITCHER_KEY_FN    0xE9
+        #define LAYER_SWITCHER_KEY_MOUSE 0xEA
 
         #define KEYMATRICES_NUM 2
         #define KEYMATRIX_LEFT  (KeyMatrices + 0)
         #define KEYMATRIX_RIGHT (KeyMatrices + 1)
 
     /* External Variables: */
-        extern const __flash uint8_t KeyboardLayout[ROWS_NUM][TOTAL_COLS_NUM][KEYMAPS_NUM][ITEM_NUM_PER_KEY];
+        extern const __flash uint8_t KeyboardLayout[ROWS_NUM][TOTAL_COLS_NUM][LAYERS_NUM][ITEM_NUM_PER_KEY];
         extern KeyMatrix_t KeyMatrices[KEYMATRICES_NUM];
 
     /* Function Prototypes: */
--- right/KeyboardReports.c
+++ right/KeyboardReports.c
@@ -5,7 +5,7 @@
 
 #include "KeyboardReports.h"
 
-uint8_t PreviousKeymap = KEYMAP_ID_NORMAL;
+uint8_t PreviousLayer = LAYER_ID_NORMAL;
 uint8_t PreviousModifiers = NO_ARGUMENT;
 
 bool CreateKeyboardHIDReport(void* ReportData, uint16_t* const ReportSize)
@@ -21,8 +21,8 @@ bool CreateKeyboardHIDReport(void* ReportData, uint16_t* const ReportSize)
     // Update the right keyboard matrix.
     KeyMatrix_Scan(KEYMATRIX_RIGHT, NULL);
 
-    // Figure out which keymap is active.
-    uint8_t ActiveKeymap = KEYMAP_ID_NORMAL;
+    // Figure out which layer is active.
+    uint8_t ActiveLayer = LAYER_ID_NORMAL;
     uint8_t ColIndex = 0;
 
     for (uint8_t MatrixId=0; MatrixId<KEYMATRICES_NUM; MatrixId++) {
@@ -32,13 +32,13 @@ bool CreateKeyboardHIDReport(void* ReportData, uint16_t* const ReportSize)
         for (uint8_t Row=0; Row<RowNum; Row++) {
             for (uint8_t Col=0; Col<ColNum; Col++) {
                 if (GET_KEY_STATE_CURRENT(KeyMatrix_GetElement(KeyMatrix, Row, Col))) {
-                    uint8_t Action = KeyboardLayout[Row][Col+ColIndex][KEYMAP_ID_NORMAL][KEY_ACTION];
-                    if (Action == VIRTUAL_MODIFIER_KEY_MOUSE) {
-                        ActiveKeymap = KEYMAP_ID_MOUSE;
-                    } else if (Action == VIRTUAL_MODIFIER_KEY_FN && ActiveKeymap != KEYMAP_ID_MOUSE) {
-                        ActiveKeymap = KEYMAP_ID_FN;
-                    } else if (Action == VIRTUAL_MODIFIER_KEY_MOD && ActiveKeymap != KEYMAP_ID_MOUSE && ActiveKeymap != KEYMAP_ID_FN) {
-                        ActiveKeymap = KEYMAP_ID_MOD;
+                    uint8_t Action = KeyboardLayout[Row][Col+ColIndex][LAYER_ID_NORMAL][KEY_ACTION];
+                    if (Action == LAYER_SWITCHER_KEY_MOUSE) {
+                        ActiveLayer = LAYER_ID_MOUSE;
+                    } else if (Action == LAYER_SWITCHER_KEY_FN && ActiveLayer != LAYER_ID_MOUSE) {
+                        ActiveLayer = LAYER_ID_FN;
+                    } else if (Action == LAYER_SWITCHER_KEY_MOD && ActiveLayer != LAYER_ID_MOUSE && ActiveLayer != LAYER_ID_FN) {
+                        ActiveLayer = LAYER_ID_MOD;
                     }
                 }
             }
@@ -57,9 +57,9 @@ bool CreateKeyboardHIDReport(void* ReportData, uint16_t* const ReportSize)
                 uint8_t KeyState = KeyMatrix_GetElement(KeyMatrix, Row, Col);
                 if (GET_KEY_STATE_CURRENT(KeyState)) {
                     // TODO: Remove "const __flash" after putting the layout into the SRAM.
-                    const __flash uint8_t (*Key)[KEYMAPS_NUM][ITEM_NUM_PER_KEY] = &KeyboardLayout[Row][Col+ColIndex];
-                    const __flash uint8_t *ActiveKey = (*Key)[ActiveKeymap];
-                    const __flash uint8_t *NormalKey = (*Key)[KEYMAP_ID_NORMAL];
+                    const __flash uint8_t (*Key)[LAYERS_NUM][ITEM_NUM_PER_KEY] = &KeyboardLayout[Row][Col+ColIndex];
+                    const __flash uint8_t *ActiveKey = (*Key)[ActiveLayer];
+                    const __flash uint8_t *NormalKey = (*Key)[LAYER_ID_NORMAL];
 
                     uint8_t Action = ActiveKey[KEY_ACTION];
                     uint8_t Argument = ActiveKey[KEY_ARGUMENT];
@@ -71,12 +71,12 @@ bool CreateKeyboardHIDReport(void* ReportData, uint16_t* const ReportSize)
                         KeyboardReport->Modifier |= NormalKey[KEY_ARGUMENT];
                     } else if  // Check the keys of the actual keymap.
                        (Action != NO_ACTION &&
-                        Action != VIRTUAL_MODIFIER_KEY_MOUSE &&
-                        Action != VIRTUAL_MODIFIER_KEY_FN &&
-                        Action != VIRTUAL_MODIFIER_KEY_MOD)
+                        Action != LAYER_SWITCHER_KEY_MOUSE &&
+                        Action != LAYER_SWITCHER_KEY_FN &&
+                        Action != LAYER_SWITCHER_KEY_MOD)
                     {
-                        // Suppress keys upon virtual modifier release.
-                        if (PreviousKeymap != KEYMAP_ID_NORMAL && ActiveKeymap == KEYMAP_ID_NORMAL) {
+                        // Suppress keys upon layer switcher key release.
+                        if (PreviousLayer != LAYER_ID_NORMAL && ActiveLayer == LAYER_ID_NORMAL) {
                             KeyState |= KEY_STATE_MASK_SUPPRESSED;
                             KeyMatrix_SetElement(KeyMatrix, Row, Col, KeyState);
                         }
@@ -99,16 +99,16 @@ bool CreateKeyboardHIDReport(void* ReportData, uint16_t* const ReportSize)
         ColIndex += ColNum;
     }
 
-    // When a virtual modifier gets pressed along with another key that produces some actual
-    // modifiers and the accomanying key gets released then keep the related actual modifiers active
-    // as long as the virtual modifier gets released.  Useful for alt-tab alternatives and the like.
-    if (ActiveKeymap != KEYMAP_ID_NORMAL && ActiveKeymap == PreviousKeymap && UsedKeyCodes == 0) {
+    // When a layer switcher key gets pressed along with another key that produces some modifiers
+    // and the accomanying key gets released then keep the related modifiers active a long as the
+    // layer switcher key stays pressed.  Useful for alt-tab alternatives and the like.
+    if (ActiveLayer != LAYER_ID_NORMAL && ActiveLayer == PreviousLayer && UsedKeyCodes == 0) {
         KeyboardReport->Modifier |= PreviousModifiers;
     }
 
     *ReportSize = sizeof(USB_KeyboardReport_Data_t);
 
-    PreviousKeymap = ActiveKeymap;
+    PreviousLayer = ActiveLayer;
     PreviousModifiers = KeyboardReport->Modifier;
 
     return false;
