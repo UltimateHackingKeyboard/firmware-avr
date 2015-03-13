9c153a0f47ee6affe8fa93239c61179a4a34f255
László Monda <laci@monda.hu>
2015-03-13 15:16:42 +0100

    Exchange order for improved code clarity.

--- left/PCA9634.c
+++ left/PCA9634.c
@@ -11,9 +11,8 @@ void PCA9634_Init()
 
   TWI_Start();
   TWI_Write(PCA9634_ADDRESS);
-  TWI_Write(PCA9634_AI_ALL | PCA9634_REG_LEDOUT0);
+  TWI_Write(PCA9634_REG_LEDOUT0 | PCA9634_AI_ALL);
   TWI_Write(0b10101010);  // Enable PWM mode for PWM0 to PWM3.
   TWI_Write(0b10101010);  // Enable PWM mode for PWM4 to PWM7.
   TWI_Stop();
 }
-
