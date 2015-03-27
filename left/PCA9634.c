b269ee0a0a40c16e7caf98a82da0f00d09b17954
László Monda <laci@monda.hu>
2015-03-27 20:09:31 +0100

    Fix indentation.

--- left/PCA9634.c
+++ left/PCA9634.c
@@ -2,17 +2,17 @@
 
 void PCA9634_Init()
 {
-  // Enable internal oscillator.
-  TWI_Start();
-  TWI_Write(PCA9634_ADDRESS);
-  TWI_Write(PCA9634_REG_MODE1);
-  TWI_Write(0b00001);
-  TWI_Stop();
+    // Enable internal oscillator.
+    TWI_Start();
+    TWI_Write(PCA9634_ADDRESS);
+    TWI_Write(PCA9634_REG_MODE1);
+    TWI_Write(0b00001);
+    TWI_Stop();
 
-  TWI_Start();
-  TWI_Write(PCA9634_ADDRESS);
-  TWI_Write(PCA9634_REG_LEDOUT0 | PCA9634_AI_ALL);
-  TWI_Write(0b10101010);  // Enable PWM mode for PWM0 to PWM3.
-  TWI_Write(0b10101010);  // Enable PWM mode for PWM4 to PWM7.
-  TWI_Stop();
+    TWI_Start();
+    TWI_Write(PCA9634_ADDRESS);
+    TWI_Write(PCA9634_REG_LEDOUT0 | PCA9634_AI_ALL);
+    TWI_Write(0b10101010);  // Enable PWM mode for PWM0 to PWM3.
+    TWI_Write(0b10101010);  // Enable PWM mode for PWM4 to PWM7.
+    TWI_Stop();
 }
