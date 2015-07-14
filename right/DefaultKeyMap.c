6129f56d9b9781610bc27708994afedcfe828839
László Monda <laci@monda.hu>
2015-07-14 03:33:38 +0200

    Add volume up and down keys.

--- right/DefaultKeyMap.c
+++ right/DefaultKeyMap.c
@@ -67,12 +67,12 @@ const __flash uint8_t KeyMap[ROWS_NUM][TOTAL_COLS_NUM][LAYERS_NUM][ITEM_NUM_PER_
         }, /* A12 key */ {
             /* base layer  */ { HID_KEYBOARD_SC_MINUS_AND_UNDERSCORE,              NO_ARGUMENT },
             /* mod layer   */ { HID_KEYBOARD_SC_F11,                               NO_ARGUMENT },
-            /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
+            /* fn layer    */ { HID_KEYBOARD_SC_VOLUME_DOWN,                       NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
         }, /* A13 key */ {
             /* base layer  */ { HID_KEYBOARD_SC_EQUAL_AND_PLUS,                    NO_ARGUMENT },
             /* mod layer   */ { HID_KEYBOARD_SC_F12,                               NO_ARGUMENT },
-            /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
+            /* fn layer    */ { HID_KEYBOARD_SC_VOLUME_UP,                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
         }, /* A14 key */ {
             /* base layer  */ { HID_KEYBOARD_SC_BACKSPACE,                         NO_ARGUMENT },
