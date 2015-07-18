7e231fc599205875014b4fa0694f076919d68ed8
László Monda <laci@monda.hu>
2015-07-18 22:59:22 +0200

    Exchange the middle and right mouse buttons.

--- right/KeyboardReports.h
+++ right/KeyboardReports.h
@@ -14,8 +14,8 @@
         #define MOUSE_STATE_RIGHT (1<<3)
 
         #define MOUSE_STATE_LEFT_CLICK   (1<<0)
-        #define MOUSE_STATE_MIDDLE_CLICK (1<<1)
-        #define MOUSE_STATE_RIGHT_CLICK  (1<<2)
+        #define MOUSE_STATE_MIDDLE_CLICK (1<<2)
+        #define MOUSE_STATE_RIGHT_CLICK  (1<<1)
         #define MOUSE_STATE_SCROLL_UP    (1<<3)
         #define MOUSE_STATE_SCROLL_DOWN  (1<<4)
 
