f49bdd40b993c9e7563300f5f68d899a15469aae
László Monda <laci@monda.hu>
2015-07-28 14:34:18 +0200

    Move Fn keys to the edges and rearrange other modifier keys accordingly.

--- right/DefaultKeyMap.c
+++ right/DefaultKeyMap.c
@@ -298,22 +298,22 @@ const __flash uint8_t KeyMap[ROWS_NUM][TOTAL_COLS_NUM][LAYERS_NUM][ITEM_NUM_PER_
         }
     }, /* key matrix row 5 */ {
         { /* E1 key */
-            /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_LEFTCTRL },
+            /* base layer  */ { LAYER_SWITCHER_KEY_FN,                             NO_ARGUMENT },
             /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
         }, /* E2 key */ {
-            /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_LEFTALT },
+            /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_LEFTCTRL },
             /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
         }, /* E3 key */ {
-            /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_LEFTGUI },
+            /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_LEFTALT },
             /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
         }, /* E4 key */ {
-            /* base layer  */ { LAYER_SWITCHER_KEY_FN,                             NO_ARGUMENT },
+            /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_LEFTGUI },
             /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
@@ -348,22 +348,22 @@ const __flash uint8_t KeyMap[ROWS_NUM][TOTAL_COLS_NUM][LAYERS_NUM][ITEM_NUM_PER_
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
         }, /* E7 key */ {
-            /* base layer  */ { LAYER_SWITCHER_KEY_FN,                             NO_ARGUMENT },
-            /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
-            /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
-            /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
-        }, /* E8 key */ {
             /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_RIGHTGUI },
             /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
-        }, /* E9 key */ {
+        }, /* E8 key */ {
             /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_RIGHTALT },
             /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
-        }, /* E10 key */ {
+        }, /* E9 key */ {
             /* base layer  */ { NO_ACTION,                                         HID_KEYBOARD_MODIFIER_RIGHTCTRL },
+            /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
+            /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
+            /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
+        }, /* E10 key */ {
+            /* base layer  */ { LAYER_SWITCHER_KEY_FN,                             NO_ARGUMENT },
             /* mod layer   */ { NO_ACTION,                                         NO_ARGUMENT },
             /* fn layer    */ { NO_ACTION,                                         NO_ARGUMENT },
             /* mouse layer */ { NO_ACTION,                                         NO_ARGUMENT }
